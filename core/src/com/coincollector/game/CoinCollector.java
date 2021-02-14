abe3d46df99b3c3a8c537333762dd5f16d853a95
Faruk Colak <faruk_colak53@hotmail.com>
2021-02-14 17:48:00 +0300

    bomb and coin added. detects collisions

--- core/src/com/coincollector/game/CoinCollector.java
+++ core/src/com/coincollector/game/CoinCollector.java
@@ -4,7 +4,12 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class CoinCollector extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -15,7 +20,27 @@ public class CoinCollector extends ApplicationAdapter {
 	float gravity = 0.2f;  //falling
 	float velocity = 0;
 	float heroY = 0;  //y coordinate of hero
+	Random random;
 
+	//Coordinates of coins
+	ArrayList<Integer> coinX = new ArrayList<>();
+	ArrayList<Integer> coinY = new ArrayList<>();
+	Texture coin;
+	int coinCount;
+
+	//Coordinates of bombs
+	ArrayList<Integer> bombX = new ArrayList<>();
+	ArrayList<Integer> bombY = new ArrayList<>();
+	Texture bomb;
+	int bombCount;
+
+	//Coin boundaries to detect collisions
+	ArrayList<Rectangle> coinRectangles = new ArrayList<>();
+
+	//Bomb boundaries to detect collisions
+	ArrayList<Rectangle> bombRectangles = new ArrayList<>();
+
+	Rectangle heroRectangle;
 
 	@Override
 	public void create () {
@@ -26,8 +51,25 @@ public class CoinCollector extends ApplicationAdapter {
 		hero[1] = new Texture("frame-2.png");
 		hero[2] = new Texture("frame-3.png");
 		hero[3] = new Texture("frame-4.png");
+		coin = new Texture("coin.png");
+		bomb = new Texture("bomb.png");
+		random = new Random();
 
 		heroY = Gdx.graphics.getHeight()/2;
+	}
+
+	public void makeCoin(){
+		float height = random.nextFloat() * Gdx.graphics.getHeight();
+		coinY.add((int) height);
+
+		coinX.add(Gdx.graphics.getWidth());
+	}
+
+	public void makeBomb(){
+		float height = random.nextFloat() * Gdx.graphics.getHeight();
+		bombY.add((int) height);
+
+		bombX.add(Gdx.graphics.getWidth());
 	}
 
 	@Override
@@ -36,8 +78,36 @@ public class CoinCollector extends ApplicationAdapter {
 
 		batch.draw(background,0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+		// Make bomb interval
+		if (bombCount < 150)
+			bombCount++;
+		else {
+			bombCount = 0;
+			makeBomb();
+		}
+
+		for (int i = 0; i < bombX.size(); i++){
+			batch.draw(bomb, bombX.get(i), bombY.get(i));
+			bombX.set(i, bombX.get(i)-8); //Moves bomb to left
+			bombRectangles.add(new Rectangle(bombX.get(i), bombY.get(i), bomb.getWidth(), bomb.getHeight()));
+		}
+
 		if (Gdx.input.justTouched()){  //For every tap, jump +10
 			velocity = -10;
+		}
+
+		// Make coin interval
+		if (coinCount < 150)
+			coinCount++;
+		else {
+			coinCount = 0;
+			makeCoin();
+		}
+		coinRectangles.clear();
+		for (int i = 0; i < coinX.size(); i++){
+			batch.draw(coin, coinX.get(i), coinY.get(i));
+			coinX.set(i, coinX.get(i)-4); //Moves coin to left
+			coinRectangles.add(new Rectangle(coinX.get(i), coinY.get(i), coin.getWidth(), coin.getHeight()));
 		}
 
 		if (pause < 8){
@@ -61,6 +131,21 @@ public class CoinCollector extends ApplicationAdapter {
 		velocity = velocity + gravity;
 		heroY = heroY - velocity;
 
+		heroRectangle = new Rectangle(Gdx.graphics.getWidth()/2 - hero[heroState].getWidth()/2, (int) heroY, hero[heroState].getWidth(), hero[heroState].getHeight());
+
+		//Detect collisions between coin and hero
+		for (int i = 0; i < coinRectangles.size(); i++){
+			if (Intersector.overlaps(heroRectangle, coinRectangles.get(i))){
+				Gdx.app.log("coin","collision");
+			}
+		}
+
+		//Detect collisions between bomb and hero
+		for (int i = 0; i < bombRectangles.size(); i++){
+			if (Intersector.overlaps(heroRectangle, bombRectangles.get(i))){
+				Gdx.app.log("bomb","collision");
+			}
+		}
 		batch.end();
 	}
 	
